64e14068f199b87c2b875562aebbeebec2e31595
chumnita <chumnita0312@gmail.com>
2025-04-04 09:38:49 +0700

    TP01 done

---
+++ src/main/java/itc/gica/App.java
@@ -0,0 +1,10 @@
+package itc.gica;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/itc/gica/AppTest.java
@@ -0,0 +1,19 @@
+package itc.gica;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
